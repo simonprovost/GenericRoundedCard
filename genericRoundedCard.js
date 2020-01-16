f074f3f023463ad108a95cbfa891602482cf5af0
SimonProvost <simon1.provost@epitech.eu>
2020-01-16 16:30:30 +0100

    feat(master): copy all props into a state was uselees, I've deleted this useless process. And updated the example as well.

--- genericRoundedCard.js
+++ genericRoundedCard.js
@@ -7,7 +7,7 @@ import * as Font from 'expo-font';
 export class GenericRoundedCard extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
+		this.state.= {
 			loading: true,
 		};
 	}
@@ -22,51 +22,6 @@ export class GenericRoundedCard extends Component {
 		});
 		this.setState({
 			loading: false,
-			backgroundColorCard: this.props.backgroundColorCard,
-
-			thumbnailTopLeftCorner: this.props.thumbnailTopLeftCorner,
-			formatPictureTopLeftCorner: this.props.formatPictureTopLeftCorner,
-			fontTopLeftCornerTitle: this.props.fontTopLeftCornerTitle,
-			fontTopLeftCornerText: this.props.fontTopLeftCornerText,
-			contentTopLeftCornerTitle: this.props.contentTopLeftCornerTitle,
-			contentTopLeftCornerText: this.props.contentTopLeftCornerText,
-			sizeTitleTopLeftCorner: this.props.sizeTitleTopLeftCorner,
-			sizeTextTopLeftCorner: this.props.sizeTextTopLeftCorner,
-			colorTopLeftCornerTitle: this.props.colorTopLeftCornerTitle,
-			colorTopLeftCornerText: this.props.colorTopLeftCornerText,
-
-			thumbnailTopRightCorner: this.props.thumbnailTopRightCorner,
-			formatPictureTopRightCorner: this.props.formatPictureTopRightCorner,
-			fontTopRightCornerTitle: this.props.fontTopRightCornerTitle,
-			fontTopRightCornerText: this.props.fontTopRightCornerText,
-			contentTopRightCornerTitle: this.props.contentTopRightCornerTitle,
-			contentTopRightCornerText: this.props.contentTopRightCornerText,
-			sizeTitleTopRightCorner: this.props.sizeTitleTopRightCorner,
-			sizeTextTopRightCorner: this.props.sizeTextTopRightCorner,
-			colorTopRightCornerTitle: this.props.colorTopRightCornerTitle,
-			colorTopRightCornerText: this.props.colorTopRightCornerText,
-
-			thumbnailBottomLeftCorner: this.props.thumbnailBottomLeftCorner,
-			formatPictureBottomLeft: this.props.formatPictureBottomLeft,
-			fontBottomLeftTitle: this.props.fontBottomLeftTitle,
-			fontBottomLeftText: this.props.fontBottomLeftText,
-			contentBottomLeftTitle: this.props.contentBottomLeftTitle,
-			contentBottomLeftText: this.props.contentBottomLeftText,
-			sizeTitleBottomLeft: this.props.sizeTitleBottomLeft,
-			sizeTextBottomLeft: this.props.sizeTextBottomLeft,
-			colorBottomLeftTitle: this.props.colorBottomLeftTitle,
-			colorBottomLeftText: this.props.colorBottomLeftText,
-
-			thumbnailBottomRightCorner: this.props.thumbnailBottomRightCorner,
-			formatPictureBottomRight: this.props.formatPictureBottomRight,
-			fontBottomRightTitle: this.props.fontBottomRightTitle,
-			fontBottomRightText: this.props.fontBottomRightText,
-			contentBottomRightTitle: this.props.contentBottomRightTitle,
-			contentBottomRightText: this.props.contentBottomRightText,
-			sizeTitleBottomRight: this.props.sizeTitleBottomRight,
-			sizeTextBottomRight: this.props.sizeTextBottomRight,
-			colorBottomRightTitle: this.props.colorBottomRightTitle,
-			colorBottomRightText: this.props.colorBottomRightText,
 		});
 	}
 
@@ -79,7 +34,7 @@ export class GenericRoundedCard extends Component {
 			);
 		}
 
-		if (this.state.backgroundColorCard === undefined) {
+		if (this.props.backgroundColorCard === undefined) {
 			return (
 				<View>
 					<Text>ERROR Background not found</Text>
@@ -88,105 +43,105 @@ export class GenericRoundedCard extends Component {
 		}
 
 		let headerLeftContent =
-			this.state.thumbnailTopLeftCorner !== undefined && this.state.formatPictureTopLeftCorner === 'square'
+			this.props.thumbnailTopLeftCorner !== undefined && this.props.formatPictureTopLeftCorner === 'square'
 				?
 				<Left style={{flex: 0, flexDirection: "column"}}>
-					<Thumbnail square source={{uri: this.state.thumbnailTopLeftCorner}} />
+					<Thumbnail square source={{uri: this.props.thumbnailTopLeftCorner}} />
 				</Left>
 				:
-				this.state.thumbnailTopLeftCorner !== undefined && this.state.formatPictureTopLeftCorner !== 'square'
+				this.props.thumbnailTopLeftCorner !== undefined && this.props.formatPictureTopLeftCorner !== 'square'
 					?
 					<Left style={{flex: 0, flexDirection: "column"}}>
-						<Thumbnail source={{uri: this.state.thumbnailTopLeftCorner}} />
+						<Thumbnail source={{uri: this.props.thumbnailTopLeftCorner}} />
 					</Left>
 					:
-					this.state.contentTopLeftCornerText !== undefined
+					this.props.contentTopLeftCornerText !== undefined
 						?
 						<Left style={{flex: 0, flexDirection: "column"}}>
-							<Text style={{fontSize: this.state.sizeTitleTopLeftCorner, fontFamily: this.state.fontTopLeftCornerTitle, color: this.state.colorTopLeftCornerTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.state.contentTopLeftCornerTitle}</Text>
-							<Text note style={{fontSize: this.state.sizeTextTopLeftCorner, fontFamily: this.state.fontTopLeftCornerText, color: this.state.colorTopLeftCornerText, textAlign: 'left', alignSelf: 'stretch'}}>{this.state.contentTopLeftCornerText}</Text>
+							<Text style={{fontSize: this.props.sizeTitleTopLeftCorner, fontFamily: this.props.fontTopLeftCornerTitle, color: this.props.colorTopLeftCornerTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.props.contentTopLeftCornerTitle}</Text>
+							<Text note style={{fontSize: this.props.sizeTextTopLeftCorner, fontFamily: this.props.fontTopLeftCornerText, color: this.props.colorTopLeftCornerText, textAlign: 'left', alignSelf: 'stretch'}}>{this.props.contentTopLeftCornerText}</Text>
 						</Left>
 						:
 						<Left style={{flex: 0, flexDirection: "column"}}>
-							<Text style={{fontSize: this.state.sizeTitleTopLeftCorner, fontFamily: this.state.fontTopLeftCornerTitle, color: this.state.colorTopLeftCornerTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.state.contentTopLeftCornerTitle}</Text>
+							<Text style={{fontSize: this.props.sizeTitleTopLeftCorner, fontFamily: this.props.fontTopLeftCornerTitle, color: this.props.colorTopLeftCornerTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.props.contentTopLeftCornerTitle}</Text>
 						</Left> ;
 
 		let headerRightContent =
-			this.state.thumbnailTopRightCorner !== undefined && this.state.formatPictureTopRightCorner === 'square'
+			this.props.thumbnailTopRightCorner !== undefined && this.props.formatPictureTopRightCorner === 'square'
 				?
 				<Right style={{flex: 1, flexDirection: "column"}}>
-					<Thumbnail square source={{uri: this.state.thumbnailTopRightCorner}} />
+					<Thumbnail square source={{uri: this.props.thumbnailTopRightCorner}} />
 				</Right>
 				:
-				this.state.thumbnailTopRightCorner !== undefined && this.state.formatPictureTopRightCorner !== 'square'
+				this.props.thumbnailTopRightCorner !== undefined && this.props.formatPictureTopRightCorner !== 'square'
 					?
 					<Right style={{flex: 1, flexDirection: "column"}}>
-						<Thumbnail source={{uri: this.state.thumbnailTopRightCorner}} />
+						<Thumbnail source={{uri: this.props.thumbnailTopRightCorner}} />
 					</Right>
 					:
-					this.state.contentTopRightCornerText !== undefined
+					this.props.contentTopRightCornerText !== undefined
 						?
 						<Right style={{flex: 1, flexDirection: "column"}}>
-							<Text style={{fontFamily: this.state.fontTopRightCornerTitle, color: this.state.colorTopRightCornerTitle, textAlign: 'right', alignSelf: 'stretch', fontSize: this.state.sizeTitleTopRightCorner}}>{this.state.contentTopRightCornerTitle}</Text>
-							<Text note style={{fontFamily: this.state.fontTopRightCornerText, color: this.state.colorTopRightCornerText, textAlign: 'right', alignSelf: 'stretch', fontSize: this.state.sizeTextTopRightCorner}}>{this.state.contentTopRightCornerText}</Text>
+							<Text style={{fontFamily: this.props.fontTopRightCornerTitle, color: this.props.colorTopRightCornerTitle, textAlign: 'right', alignSelf: 'stretch', fontSize: this.props.sizeTitleTopRightCorner}}>{this.props.contentTopRightCornerTitle}</Text>
+							<Text note style={{fontFamily: this.props.fontTopRightCornerText, color: this.props.colorTopRightCornerText, textAlign: 'right', alignSelf: 'stretch', fontSize: this.props.sizeTextTopRightCorner}}>{this.props.contentTopRightCornerText}</Text>
 						</Right>
 						:
 						<Right style={{flex: 1, flexDirection: "column"}}>
-							<Text style={{fontFamily: this.state.fontTopRightCornerTitle, color: this.state.colorTopRightCornerTitle, textAlign: 'right', alignSelf: 'stretch', fontSize: this.state.sizeTitleTopRightCorner}}>{this.state.contentTopRightCornerTitle}</Text>
-							<Text note style={{fontFamily: this.state.fontTopRightCornerText, color: this.state.colorTopRightCornerText, textAlign: 'right', alignSelf: 'stretch', fontSize: this.state.sizeTextTopRightCorner}}>{this.state.contentTopRightCornerText}</Text>
+							<Text style={{fontFamily: this.props.fontTopRightCornerTitle, color: this.props.colorTopRightCornerTitle, textAlign: 'right', alignSelf: 'stretch', fontSize: this.props.sizeTitleTopRightCorner}}>{this.props.contentTopRightCornerTitle}</Text>
+							<Text note style={{fontFamily: this.props.fontTopRightCornerText, color: this.props.colorTopRightCornerText, textAlign: 'right', alignSelf: 'stretch', fontSize: this.props.sizeTextTopRightCorner}}>{this.props.contentTopRightCornerText}</Text>
 						</Right>;
 
 		let BottomLeftCornerContent =
-			this.state.thumbnailBottomLeftCorner !== undefined && this.state.formatPictureBottomLeft === 'square'
+			this.props.thumbnailBottomLeftCorner !== undefined && this.props.formatPictureBottomLeft === 'square'
 				?
 				<Left style={{flex: 0, flexDirection: "column"}}>
-					<Thumbnail square source={{uri: this.state.thumbnailBottomLeftCorner}} />
+					<Thumbnail square source={{uri: this.props.thumbnailBottomLeftCorner}} />
 				</Left>
 				:
-				this.state.thumbnailBottomLeftCorner !== undefined && this.state.formatPictureBottomLeft !== 'square'
+				this.props.thumbnailBottomLeftCorner !== undefined && this.props.formatPictureBottomLeft !== 'square'
 					?
 					<Left style={{flex: 0, flexDirection: "column"}}>
-						<Thumbnail source={{uri: this.state.thumbnailBottomLeftCorner}} />
+						<Thumbnail source={{uri: this.props.thumbnailBottomLeftCorner}} />
 					</Left>
 					:
-					this.state.contentBottomLeftText !== undefined
+					this.props.contentBottomLeftText !== undefined
 						?
 						<Left style={{flex: 0, flexDirection: "column"}}>
-							<Text style={{fontFamily: this.state.fontBottomLeftTitle, color: this.state.colorBottomLeftTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.state.contentBottomLeftTitle}</Text>
-							<Text note style={{fontFamily: this.state.fontBottomLeftText, color: this.state.colorBottomLeftText, textAlign: 'left', alignSelf: 'stretch'}}>{this.state.contentBottomLeftText}</Text>
+							<Text style={{fontFamily: this.props.fontBottomLeftTitle, color: this.props.colorBottomLeftTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.props.contentBottomLeftTitle}</Text>
+							<Text note style={{fontFamily: this.props.fontBottomLeftText, color: this.props.colorBottomLeftText, textAlign: 'left', alignSelf: 'stretch'}}>{this.props.contentBottomLeftText}</Text>
 						</Left>
 						:
 						<Left style={{flex: 0, flexDirection: "column"}}>
-							<Text style={{fontFamily: this.state.fontBottomLeftTitle, color: this.state.colorBottomLeftTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.state.contentBottomLeftTitle}</Text>
+							<Text style={{fontFamily: this.props.fontBottomLeftTitle, color: this.props.colorBottomLeftTitle, textAlign: 'left', alignSelf: 'stretch'}}>{this.props.contentBottomLeftTitle}</Text>
 						</Left>;
 
 		let BottomRightCornerContent =
-			this.state.thumbnailBottomRightCorner !== undefined && this.state.formatPictureBottomRight === 'square'
+			this.props.thumbnailBottomRightCorner !== undefined && this.props.formatPictureBottomRight === 'square'
 				?
 				<Right style={{flex: 1, flexDirection: "column"}}>
-					<Thumbnail square source={{uri: this.state.thumbnailBottomRightCorner}} />
+					<Thumbnail square source={{uri: this.props.thumbnailBottomRightCorner}} />
 				</Right>
 				:
-				this.state.thumbnailBottomRightCorner !== undefined && this.state.formatPictureBottomRight !== 'square'
+				this.props.thumbnailBottomRightCorner !== undefined && this.props.formatPictureBottomRight !== 'square'
 					?
 					<Right style={{flex: 1, flexDirection: "column"}}>
-						<Thumbnail source={{uri: this.state.thumbnailBottomRightCorner}} />
+						<Thumbnail source={{uri: this.props.thumbnailBottomRightCorner}} />
 					</Right>
 					:
-					this.state.contentBottomRightText !== undefined
+					this.props.contentBottomRightText !== undefined
 						?
 						<Right style={{flex: 1, flexDirection: "column"}}>
-							<Text style={{fontFamily: this.state.fontBottomRightTitle, color: this.state.colorBottomRightTitle, textAlign: 'right', alignSelf: 'stretch'}}>{this.state.contentBottomRightTitle}</Text>
-							<Text note style={{fontFamily: this.state.fontBottomRightText, color: this.state.colorBottomRightText, textAlign: 'right', alignSelf: 'stretch'}}>{this.state.contentBottomRightText}</Text>
+							<Text style={{fontFamily: this.props.fontBottomRightTitle, color: this.props.colorBottomRightTitle, textAlign: 'right', alignSelf: 'stretch'}}>{this.props.contentBottomRightTitle}</Text>
+							<Text note style={{fontFamily: this.props.fontBottomRightText, color: this.props.colorBottomRightText, textAlign: 'right', alignSelf: 'stretch'}}>{this.props.contentBottomRightText}</Text>
 						</Right>
 						:
 						<Right style={{flex: 1, flexDirection: "column"}}>
-							<Text style={{fontFamily: this.state.fontBottomRightTitle, color: this.state.colorBottomRightTitle, textAlign: 'right', alignSelf: 'stretch'}}>{this.state.contentBottomRightTitle}</Text>
-							<Text note style={{fontFamily: this.state.fontBottomRightText, color: this.state.colorBottomRightText, textAlign: 'right', alignSelf: 'stretch'}}>{this.state.contentBottomRightText}</Text>
+							<Text style={{fontFamily: this.props.fontBottomRightTitle, color: this.props.colorBottomRightTitle, textAlign: 'right', alignSelf: 'stretch'}}>{this.props.contentBottomRightTitle}</Text>
+							<Text note style={{fontFamily: this.props.fontBottomRightText, color: this.props.colorBottomRightText, textAlign: 'right', alignSelf: 'stretch'}}>{this.props.contentBottomRightText}</Text>
 						</Right>;
 
 		let emptySpace =
-			<CardItem style={{borderTopLeftRadius: 0, borderTopRightRadius: 0, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, backgroundColor: this.state.backgroundColorCard }}>
+			<CardItem style={{borderTopLeftRadius: 0, borderTopRightRadius: 0, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, backgroundColor: this.props.backgroundColorCard }}>
 				<Body>
 				</Body>
 			</CardItem>;
@@ -195,7 +150,7 @@ export class GenericRoundedCard extends Component {
 			<View style={styles.container}>
 				<View padder>
 					<Card style={{borderRadius: 9}}>
-						<CardItem header style={{borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderTopLeftRadius: 9, borderTopRightRadius: 9,  backgroundColor: this.state.backgroundColorCard, flexDirection: "row"}}>
+						<CardItem header style={{borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderTopLeftRadius: 9, borderTopRightRadius: 9,  backgroundColor: this.props.backgroundColorCard, flexDirection: "row"}}>
 							{headerLeftContent}
 							{headerRightContent}
 						</CardItem>
@@ -203,7 +158,7 @@ export class GenericRoundedCard extends Component {
 						{emptySpace}
 						{emptySpace}
 						{emptySpace}
-						<CardItem footer style={{borderBottomLeftRadius: 8, borderBottomRightRadius: 8, borderTopLeftRadius: 0, borderTopRightRadius: 0, backgroundColor: this.state.backgroundColorCard }}>
+						<CardItem footer style={{borderBottomLeftRadius: 8, borderBottomRightRadius: 8, borderTopLeftRadius: 0, borderTopRightRadius: 0, backgroundColor: this.props.backgroundColorCard }}>
 							{BottomLeftCornerContent}
 							{BottomRightCornerContent}
 						</CardItem>
